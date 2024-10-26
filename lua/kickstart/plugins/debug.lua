39c68631c891695accd165505f7cd35ad60fbf8f
Morgan Arrington <mjarring96@gmail.com>
2024-10-26 19:22:55 -0400

    Add lldb DAP configuration

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -101,5 +101,39 @@ return {
         detached = vim.fn.has 'win32' == 0,
       },
     }
+
+    -- Setup vscode-lldb
+    dap.adapters.lldb = {
+      type = 'executable',
+      command = '/Users/arrington/llvm-19.1.2/bin/lldb-dap', -- adjust as needed, must be absolute path
+      name = 'lldb',
+    }
+
+    dap.configurations.cpp = {
+      {
+        name = 'Launch',
+        type = 'lldb',
+        request = 'launch',
+        program = function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {},
+
+        -- 💀
+        -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+        --
+        --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+        --
+        -- Otherwise you might get the following error:
+        --
+        --    Error on launch: Failed to attach to the target process
+        --
+        -- But you should be aware of the implications:
+        -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+        -- runInTerminal = false,
+      },
+    }
   end,
 }
